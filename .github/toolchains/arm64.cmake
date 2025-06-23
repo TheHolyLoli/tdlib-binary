5d598578f692d16e18357d8a3843504e2677ccbf
TheHolyLoli <13733900+TheHolyLoli@users.noreply.github.com>
2025-06-23 16:33:45 +0330

    Update arm64.cmake

--- .github/toolchains/arm64.cmake
+++ .github/toolchains/arm64.cmake
@@ -9,16 +9,14 @@ set(CMAKE_CXX_COMPILER clang++-18)
 # Specify the target architecture (ARM64, aarch64)
 set(CMAKE_C_FLAGS      "--target=aarch64-linux-gnu -march=armv8-a -fPIC")
 set(CMAKE_CXX_FLAGS    "--target=aarch64-linux-gnu -march=armv8-a -fPIC")
-set(OPENSSL_ROOT_DIR /usr/aarch64-linux-gnu)
-set(OPENSSL_LIBRARIES /usr/aarch64-linux-gnu/lib)
 
 # Where the cross-compilation libraries/headers are located
 set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
 
 # Set CMake to only find libraries and includes in the cross-compilation root
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
 # Optional: Strip debug symbols (comment out if not needed)
 # set(CMAKE_EXE_LINKER_FLAGS_INIT "-s")
