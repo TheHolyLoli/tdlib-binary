f83af8ac5b7760d5620411f389e5823602e84ecc
TheHolyLoli <13733900+TheHolyLoli@users.noreply.github.com>
2025-06-23 16:34:04 +0330

    Update armhf.cmake

--- .github/toolchains/armhf.cmake
+++ .github/toolchains/armhf.cmake
@@ -13,19 +13,15 @@ set(CMAKE_CXX_COMPILER      clang++-18)
 # Cross-compile flags for ARM (armv7, hard-float, NEON/SIMD)
 set(CMAKE_C_FLAGS_INIT     "--target=arm-linux-gnueabihf -march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard")
 set(CMAKE_CXX_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT}")
-set(OPENSSL_ROOT_DIR /usr/arm-linux-gnueabihf)
-set(OPENSSL_LIBRARIES /usr/arm-linux-gnueabihf/lib)
 
 
 # Where the cross libraries/headers live (typically the sysroot path)
 set(CMAKE_FIND_ROOT_PATH   /usr/arm-linux-gnueabihf)
 
 # CMake should not search for programs in the sysroot path (use only cross-compiled libraries)
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 # Optional: Strip debug symbols to reduce size of binaries
 # set(CMAKE_EXE_LINKER_FLAGS_INIT "-s")
 # set(CMAKE_SHARED_LINKER_FLAGS_INIT "-s")
