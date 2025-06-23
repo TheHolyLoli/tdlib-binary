acee1dbcac74ec741ccb7cd0c79136db6c791078
TheHolyLoli <13733900+TheHolyLoli@users.noreply.github.com>
2025-06-23 15:57:29 +0330

    Update armhf.cmake

--- .github/toolchains/armhf.cmake
+++ .github/toolchains/armhf.cmake
@@ -12,7 +12,7 @@ set(CMAKE_CXX_COMPILER      clang++-18)
 
 # Cross-compile flags for ARM (armv7, hard-float, NEON/SIMD)
 set(CMAKE_C_FLAGS_INIT     "--target=arm-linux-gnueabihf -march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard")
-set(CMAKE_CXX_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT} -stdlib=libc++")
+set(CMAKE_CXX_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT}")
 
 # Where the cross libraries/headers live (typically the sysroot path)
 set(CMAKE_FIND_ROOT_PATH   /usr/arm-linux-gnueabihf)
